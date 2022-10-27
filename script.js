71c58a12ce1135a3d34ef1a0966e47776c2d4bce
ErikNWH <erik.nwh@gmail.com>
2022-10-27 13:49:48 +0200

    Dynamically populates the DOM with new information

--- script.js
+++ script.js
@@ -12,7 +12,51 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 
 function updateDOM() {
-  
+  resultsArray.forEach((result) => {
+    // Card container
+    const card = document.createElement('div');
+    card.classList.add('card');
+    // Link
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+    // Image
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'NASA Picture of the Day';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+    // Card body
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    // Card Title
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    // Save text
+    const save = document.createElement('p');
+    saveText.classList.add('clickable');
+    saveText.textContent = 'Add To Favorites';
+    // Card Text
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    // Footer
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    // Date
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+    // Copyright
+    const copyright = document.createElement('span');
+    copyright.textContent = ` ${result.copyright}`;
+    // Append
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+  });
 }
 
 // Get 10 images from NASA API
