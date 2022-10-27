b51b981a8326a1c23051d110d5816e5268d8cd93
ErikNWH <erik.nwh@gmail.com>
2022-10-27 11:12:58 +0200

    added Nasa API for demo purposes

---
+++ script.js
@@ -0,0 +1,29 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+function updateDOM() {
+  
+}
+
+// Get 10 images from NASA API
+async function getNasaPicutures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+    updateDOM();
+  } catch (error) {}
+}
+
+// On Load
+getNasaPicutures();
